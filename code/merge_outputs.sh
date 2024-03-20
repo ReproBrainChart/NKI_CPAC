bcb4eee44135f49082e18f2ac96e42a8278c74ce
PennLINC Informatics <matthew.cieslak@pennmedicine.upenn.edu>
2024-03-19 21:34:07 -0400

    update code/merge_outputs.sh

--- code/merge_outputs.sh
+++ code/merge_outputs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e -u -x
-outputsource=ria+file:///cbica/projects/RBC/production/NKI/c-pac-1.8.5/output_ria#003487bb-b745-4f4d-a3a3-607838cc1f13
-cd /cbica/projects/RBC/production/NKI/c-pac-1.8.5
+outputsource=ria+file:///cbica/comp_space/RBC/NKI_RERUN/c-pac-1.8.5/output_ria#003487bb-b745-4f4d-a3a3-607838cc1f13
+cd /cbica/comp_space/RBC/NKI_RERUN/c-pac-1.8.5
 
-datalad clone ${outputsource} merge_ds
+# The following should be pasted into the merge_outputs.sh script
+datalad clone ${outputsource} merge_ds -b prerun
 cd merge_ds
 NBRANCHES=$(git branch -a | grep job- | sort | wc -l)
 echo "Found $NBRANCHES branches to merge"
 
-gitref=$(git show-ref master | cut -d ' ' -f1 | head -n 1)
+gitref=$(git show-ref prerun | cut -d ' ' -f1 | head -n 1)
 
 # query all branches for the most recent commit and check if it is identical.
 # Write all branch identifiers for jobs without outputs into a file.
@@ -25,23 +26,15 @@ done | tee code/has_results.txt
 
 mkdir -p code/merge_batches
 num_branches=$(wc -l < code/has_results.txt)
-CHUNKSIZE=5000
-set +e
-num_chunks=$(expr ${num_branches} / ${CHUNKSIZE})
-if [[ $num_chunks == 0 ]]; then
-    num_chunks=1
-fi
-set -e
-for chunknum in $(seq 1 $num_chunks)
-do
-    startnum=$(expr $(expr ${chunknum} - 1) \* ${CHUNKSIZE} + 1)
-    endnum=$(expr ${chunknum} \* ${CHUNKSIZE})
-    batch_file=code/merge_branches_$(printf %04d ${chunknum}).txt
-    [[ ${num_branches} -lt ${endnum} ]] && endnum=${num_branches}
-    branches=$(sed -n "${startnum},${endnum}p;$(expr ${endnum} + 1)q" code/has_results.txt)
-    echo ${branches} > ${batch_file}
-    git merge -m "C-PAC results batch ${chunknum}/${num_chunks}" $(cat ${batch_file})
+CHUNKSIZE=200
 
+split -l ${CHUNKSIZE} --numeric-suffixes code/has_results.txt code/__results_batch
+chunks_files=$(find code -name '__results_batch*' | sort )
+num_chunks=$(echo ${chunks_files} | wc -w)
+for chunkfile in ${chunks_files}
+do
+    chunknum=$(basename ${chunkfile} | sed 's/__results_batch\([0-9][0-9]*\)/\1/')
+    git merge -m "merge results batch ${chunknum}/${num_chunks}" $(cat ${chunkfile})
 done
 
 # Push the merge back
@@ -59,9 +52,18 @@ then
     exit 1
 fi
 
-# stop tracking this branch
-git annex dead here
-
 datalad push --data nothing
-echo SUCCESS
+
+
+# Now push the results to fcp-indi
+source ~/.aws/fcpindi.sh
+git annex copy --from output-storage --to fcp-indi
+MISSING=$(git annex find --not --in fcp-indi)
+datalad push --data nothing
+
+cd ../analysis
+datalad update --how=merge -s output
+git merge main
+git checkout main
+git merge prerun
 
